976af7c042f0e49f772117cc098cfeefd910afa0
sopper08 <sopper08@gmail.com>
2019-09-19 23:26:00 +0800

    Complete hw1 part1

---
+++ hw1/Practices/r06631026_pr1/src/problem1_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int b, a, c;
+    cout << "&a: " << &a << endl;
+    cout << "&b: " << &b << endl;
+    cout << "&c: " << &c << endl;
+
+    return 0;
+}
+
+
